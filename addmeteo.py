b1abdfef9726baa6b1af2338050ccbd0ab9f375e
francisco6444 <francois.diximier@hotmail.fr>
2023-11-14 18:31:27 +0100

    yolo

--- addmeteo.py
+++ addmeteo.py
@@ -18,4 +18,24 @@ df_merged = pd.merge(df1, df2, left_on='date', right_on='DATE', how='inner')
 df_merged.drop('DATE', axis=1, inplace=True)
 
 # Enregistrer le DataFrame résultant dans un nouveau fichier Excel
+import pandas as pd
+
+# Charger les fichiers Excel dans des DataFrames
+df1 = pd.read_excel('data_tab.xlsx')
+df2 = pd.read_excel('Donnees_meteo_dayavg.xlsx')
+
+# Convertir la colonne 'timestamps_UTC' de df1 en format datetime
+df1['date'] = pd.to_datetime(df1['date'])
+
+# Convertir la colonne 'DATE' de df2 en format datetime
+df2['DATE'] = pd.to_datetime(df2['DATE'])
+
+
+# Joindre les DataFrames en fonction des dates
+df_merged = pd.merge(df1, df2, left_on='date', right_on='DATE', how='inner')
+
+# Supprimer la colonne redondante après la jointure
+df_merged.drop('DATE', axis=1, inplace=True)
+
+# Enregistrer le DataFrame résultant dans un nouveau fichier Excel
 df_merged.to_excel('resultat_jointure.xlsx', index=False)
