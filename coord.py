08adb925d0ea9607ec9386279c007cfd5ea7061a
nlermusi <nicolas.lermusiaux@etu.inp-toulouse.fr>
2023-11-14 12:49:01 +0100

    point par id

--- coord.py
+++ coord.py
@@ -1,32 +1,41 @@
 import pandas as pd
 import folium
 import os
-import os
-
-
-count = 0
+import seaborn as sns
+# extraire nombre d'un string en exp reguliere
+import re 
+trains = []
 for file in os.listdir('.'):
     if file.endswith('.csv'):
-        count += 1
-
-print(count)
+        trains.append(file)
 
 
-#nombre de fichier csv dans le dossier 
 
+colors = ['darkblue', 'blue', 'beige', 'green', 'orange', 'cadetblue', 'pink', 'lightred', 'purple', 'lightgray', 'red', 'gray', 'white', 'black', 'lightgreen', 'lightblue', 'darkpurple', 'darkred', 'darkgreen']
+
+#initialiser la carte
+
+data0 = pd.read_csv(trains[0], delimiter=',')
+number = re.search(r'\d+', trains[0]).group()
+
+carte = folium.Map(location=[data0['lat'].mean(), data0['lon'].mean()], zoom_start=8)
+
+for index, row in data0.iterrows(): 
     
+    folium.Marker([row['lat'], row['lon']], icon=folium.Icon(color=colors[0]), popup= f'Train {number}').add_to(carte)
+
+for elem, color in zip(trains[1:], colors[1:]):
 # Spécifiez le chemin vers votre fichier Excel
-chemin_fichier_excel = 'data_tab.xlsx'
+    number = re.search(r'\d+', elem).group()
 
 # Chargez le fichier Excel dans un DataFrame
-data = pd.read_excel(chemin_fichier_excel)
-
-
-carte = folium.Map(location=[data['lat'].mean(), data['lon'].mean()], zoom_start=4)
+    data = pd.read_csv(elem, delimiter=',')
 
 # Ajouter des marqueurs pour chaque lieu sur la carte
-for index, row in data.iterrows():
-    folium.Marker([row['lat'], row['lon']], popup=row['']).add_to(carte)
+
+    for index, row in data.iterrows():
+    
+        folium.Marker([row['lat'], row['lon']], icon=folium.Icon(color=color), popup= f'Train {number}').add_to(carte)
 
 # Sauvegarder la carte au format HTML
 carte.save('carte_geographique.html')
