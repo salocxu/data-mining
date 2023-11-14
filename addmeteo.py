f63fa6cedc25b5bd57a22b2e11adb3847cd72f72
nlermusi <nicolas.lermusiaux@etu.inp-toulouse.fr>
2023-11-14 17:12:21 +0100

    hdhdht

---
+++ addmeteo.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+# Charger les fichiers Excel dans des DataFrames
+df1 = pd.read_excel('C:/Users/noahl/OneDrive/Documents/ULB/INFO-H423-DataMining/data-mining/data_tab.xlsx')
+df2 = pd.read_excel('C:/Users/noahl/OneDrive/Documents/ULB/INFO-H423-DataMining/data-mining/Donnees_meteo_dayavg.xlsx')
+
+# Convertir la colonne 'timestamps_UTC' de df1 en format datetime
+df1['timestamps_UTC'] = pd.to_datetime(df1['timestamps_UTC'])
+
+# Convertir la colonne 'DATE' de df2 en format datetime
+df2['DATE'] = pd.to_datetime(df2['DATE'])
+
+
+# Joindre les DataFrames en fonction des dates
+df_merged = pd.merge(df1, df2, left_on='timestamps_UTC', right_on='DATE', how='inner')
+
+# Supprimer la colonne redondante après la jointure
+df_merged.drop('DATE', axis=1, inplace=True)
+
+# Enregistrer le DataFrame résultant dans un nouveau fichier Excel
+df_merged.to_excel('resultat_jointure.xlsx', index=False)
