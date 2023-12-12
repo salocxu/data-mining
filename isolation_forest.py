cd3c619fbfe1ec784bb38ae0a41850277b34eb48
nlermusi <nicolas.lermusiaux@etu.inp-toulouse.fr>
2023-12-12 12:11:09 +0100

    isolation

--- isolation_forest.py
+++ isolation_forest.py
@@ -6,7 +6,7 @@ import variables
 all_data = pd.read_excel('resultat_jointure2.xlsx')
 
 # choisir en consequence
-features = ['RS_E_InAirTemp_PC1', 'RS_E_InAirTemp_PC2', 'RS_T_OilTemp_PC1', 'RS_T_OilTemp_PC2', 'RS_E_WatTemp_PC1', 'RS_E_WatTemp_PC2']
+features = ['RS_E_InAirTemp_PC1', 'RS_E_InAirTemp_PC2', 'RS_T_OilTemp_PC1', 'RS_T_OilTemp_PC2', 'RS_E_WatTemp_PC1', 'RS_E_WatTemp_PC2', 'RS_E_OilPress_PC1', 'RS_E_OilPress_PC2', 'RS_E_RPM_PC1', 'RS_E_RPM_PC2']
 
 # Standardize the features
 scaler = StandardScaler()
@@ -14,6 +14,10 @@ all_data[features] = scaler.fit_transform(all_data[features])
 
 # Initialize the Isolation Forest model
 model = IsolationForest(contamination=variables.contamination)
+
+nan_rows = all_data[all_data[features].isna().any(axis=1)]
+nan_rows.to_excel('nan_rows.xlsx', index=False)
+
 
 # Fit the model to the data
 model.fit(all_data[features])
