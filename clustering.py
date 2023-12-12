60c4729c0fc3f43e13509561d609bea955e9beba
SAeleven <noahlaravine@gmail.com>
2023-12-12 12:09:25 +0100

    amelioration de clustering

--- clustering.py
+++ clustering.py
@@ -52,5 +52,45 @@ plt.scatter(new_dfa['lat'][new_dfa['labels_kmeans']==2][new_dfa['anomalie_bin']=
             new_dfa['lon'][new_dfa['labels_kmeans']==2][new_dfa['anomalie_bin']==1],
             color='red',s=20,linestyle='--')
 
+# Compter le nombre d'occurrences de 1 en fonction de la valeur de colonne2
+ano_par_ligne = new_dfa.groupby(labels).sum()
+records_par_ligne = new_dfa.groupby(labels).size()
+
+ano_l1=(ano_par_ligne.iloc[0,3])
+ano_l2=(ano_par_ligne.iloc[1,3])
+ano_l3=(ano_par_ligne.iloc[2,3])
+rec_l1=(records_par_ligne.iloc[0])
+rec_l2=(records_par_ligne.iloc[1])
+rec_l3=(records_par_ligne.iloc[2])
+
+freq_ano_l1=(ano_l1/rec_l1)*100
+freq_ano_l2=(ano_l2/rec_l2)*100
+freq_ano_l3=(ano_l3/rec_l3)*100
+
+freqtot_l1=(ano_l1/(ano_l1+ano_l2+ano_l3))*100
+freqtot_l2=(ano_l2/(ano_l1+ano_l2+ano_l3))*100
+freqtot_l3=(ano_l3/(ano_l1+ano_l2+ano_l3))*100
+
+# Afficher les résultats
+print('la fréquence danomalie sur la ligne 1 est de', freq_ano_l1)
+print('la fréquence danomalie sur la ligne 2 est de', freq_ano_l2)
+print('la fréquence danomalie sur la ligne 3 est de', freq_ano_l3)
+print('la fréquence danomalie sur la ligne 1 par rapport à toutes les anomalies est de', freqtot_l1)
+print('la fréquence danomalie sur la ligne 2 par rapport à toutes les anomalies est de', freqtot_l2)
+print('la fréquence danomalie sur la ligne 3 par rapport à toutes les anomalies est de', freqtot_l3)
+
+
+#ajout de la colonne labels au fichier csv principal
+df['labels_kmeans']=labels
+
+# Créer trois nouveaux DataFrames en fonction des labels
+cluster_0 = df[df['labels_kmeans'] == 0]
+cluster_1 = df[df['labels_kmeans'] == 1]
+cluster_2 = df[df['labels_kmeans'] == 2]
+
+# Enregistrer chaque DataFrame dans un fichier CSV
+cluster_0.to_csv('ligne_1.csv', index=False)
+cluster_1.to_csv('ligne_2.csv', index=False)
+cluster_2.to_csv('ligne_3.csv', index=False)
 
 plt.show()
